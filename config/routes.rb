a2b89d02cbe29cbecb0ca9f827f8d589b903780b
Anuj Ahuja <anujahuja1999@gmail.com>
2017-04-28 17:56:41 +0530

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  get 'home/index'
 
-	root 'application#hello'
+	root 'home#index'
+	  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
